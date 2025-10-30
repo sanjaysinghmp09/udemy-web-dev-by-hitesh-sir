62ea0a6178d14ff896ab1491a237ec32211dd1bc
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-10-30 23:03:56 +0530

    data type

---
+++ part1/dataType.js
@@ -0,0 +1,17 @@
+let firstName = "John";
+let lastName = "Doe";
+console.log(`Hello, ${firstName} ${lastName}!`);
+
+let age = 30;
+let isStudent = true;
+let hobbies = ["reading", "traveling", "coding"];   
+let address = {
+    street: "123 Main St",
+    city: "Anytown",
+    country: "USA"
+};
+console.log(typeof firstName); // string
+console.log(typeof age);       // number
+console.log(typeof isStudent); // boolean
+console.log(typeof hobbies);   // object (arrays are of type object)
+console.log(typeof address);   // object (arrays are of type object)
