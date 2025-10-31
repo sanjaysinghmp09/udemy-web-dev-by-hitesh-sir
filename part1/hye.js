c3af3d127b6e344496aeeae0957ec26eee341312
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-10-31 22:21:57 +0530

    learn operations in js

--- part1/hye.js
+++ part1/hye.js
@@ -1,1 +1,8 @@
+let userName = "Sanjay";
 console.log(`Hello, ${userName}! Welcome to the JavaScript world.`);
+
+if (userName.length > 8) {
+  console.log("Your name is quite long!");
+}else {
+    console.log("You have a nice, short name!");
+}
