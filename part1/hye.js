92fda7506bb1f1ec7971b3304f5a232d4506058c
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-01 22:00:37 +0530

    Update hye.js

--- part1/hye.js
+++ part1/hye.js
@@ -4,5 +4,5 @@ console.log(`Hello, ${userName}! Welcome to the JavaScript world.`);
 if (userName.length > 8) {
   console.log("Your name is quite long!");
 }else {
-    console.log("You have a nice, short name!");
+    console.log("You have a nice, short name! change");
 }
