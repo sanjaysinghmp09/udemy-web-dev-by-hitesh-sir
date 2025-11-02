c3aad37195cf4b2d7627174b991f31e1fcc04ba9
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-02 21:00:39 +0530

    learn premitive types in js

--- part2/nonPrimitives.js
+++ part2/nonPrimitives.js
@@ -1,2 +1,11 @@
-let obj = {};
-console.log(typeof obj);
+// let userName = {};
+// console.log(userName)
+// console.log(typeof userName);
+
+let userName = {
+    name : "Sanjay",
+    age : 21
+};
+console.log(userName.name)
+console.log(typeof userName);
+
