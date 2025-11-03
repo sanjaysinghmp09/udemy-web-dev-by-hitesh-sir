255650c088155f4dcc250d8270c063fb44dca4b0
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-03 21:46:48 +0530

    learn premitive types in js

--- part3/conditions.js
+++ part3/conditions.js
@@ -1,7 +1,46 @@
-let num1 = 5 ;
-let num2 = 8 ;
+// let num1 = 5 ;
+// let num2 = 8 ;
 
-if (num1 > num2) {
-    console.log("num1 is greater than num2");
+// if (num1 > num2) {
+//     console.log("num1 is greater than num2");
+// }else {
+//     console.log("num2 is greater than num1");
+// }
+// console.log("I am outside of if block");
+
+
+// challange2 
+
+/*
+
+let str1 = "hello";
+let str2 = "hello";
+
+if (str1 == str2) {
+    console.log("Both are equal");
 }
-console.log("I am outside of if block");
+else {
+    console.log("Both are not equal");
+}
+*/
+
+/*
+
+let bankBalance = 5000;
+
+if (typeof bankBalance === 'number') {
+    console.log('yep this is a number ');
+}else {
+    console.log('No this is not a number');
+}
+*/
+
+let arr =  [1 ,2 ,3 ];
+
+console.log(arr.length);
+
+if (arr.length == 0 ) {
+    console.log("Array is empty");
+}else {
+    console.log("Array is not empty");
+}
