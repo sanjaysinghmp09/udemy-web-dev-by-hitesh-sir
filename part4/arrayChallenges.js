85d155500e04c98561225d3a54ae7a0809d5ffaa
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-04 23:10:57 +0530

    Solve array Challenges

---
+++ part4/arrayChallenges.js
@@ -0,0 +1,14 @@
+// challenge-1 Answer
+let teaFlavors = ["black tea" , "oolong tea" ] ;
+
+let firstTea = teaFlavors[0];
+
+console.log("First tea flavor is: " + firstTea);
+
+// challenge-2 Answer "arrar start from 0 index"
+
+let cities = ["London", "Tokyo", "New York"];
+
+let favoriteCity = cities[2];
+console.log("My favorite city is: " + favoriteCity);
+
