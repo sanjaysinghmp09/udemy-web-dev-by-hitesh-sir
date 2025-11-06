c2992e4eada74a147cee17002fec002e9cd0b1c3
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-06 20:37:33 +0530

    Solve array Challenges

--- part4/arrayChallenges.js
+++ part4/arrayChallenges.js
@@ -97,3 +97,8 @@ let newStudent = ["Anushka"]
 let allStudent = studentCoaching.concat(newStudent);
 
 console.log(allStudent);
+
+const fruits = new Array("Apple", "Banana");
+
+console.log(fruits.length); // 2
+console.log(fruits[0]); // "Apple"
