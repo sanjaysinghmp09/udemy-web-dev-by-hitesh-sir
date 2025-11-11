1a813741e8dacd34fd9441d4bd0552b58ab30c70
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-11 20:00:11 +0530

    solve array problems

--- part4/levelUpLoop.js
+++ part4/levelUpLoop.js
@@ -1,22 +1,41 @@
-let teas = ['green tea' , "black tea" , "chai" , "oolong tea"];
+// challenge -1 
+let teas = ['green tea', "black tea", "chai", "oolong tea"];
 selectedTeas = [];
 for (let i = 0; i < teas.length; i++) {
-     if (teas[i] === "chai") {
-        break
-    }
-    selectedTeas.push(teas[i] 
-    )}
+   if (teas[i] === "chai") {
+      break
+   }
+   selectedTeas.push(teas[i]
+   )
+}
 // console.log(selectedTeas)
 
-let cities = ["London" , "New York" , "Paris" , "Berlin"];
+// challenge -2 
+
+let cities = ["London", "New York", "Paris", "Berlin"];
 visitedCities = [];
 
 for (let i = 0; i < cities.length; i++) {
    if (cities[i] === "Paris") {
-    continue
+      continue
    }
 
    visitedCities.push(cities[i])
-    
+
 }
-console.log(visitedCities)
+// console.log(visitedCities)
+
+// challenge -3 
+
+let numbers = [1, 2, 3, 4, 5]
+
+let smallNumbers = [];
+
+for (const num of numbers) {
+   if (num === 4) {
+      break
+   }
+
+   smallNumbers.push(num)
+}
+console.log(smallNumbers)
