1e2891c2b93efae4c9fdd4657e38342660f2aada
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-11 08:44:16 +0530

    when i solve this challenge , i motivate to solve all others problem like this

--- part4/levelUpLoop.js
+++ part4/levelUpLoop.js
@@ -1,15 +1,22 @@
 let teas = ['green tea' , "black tea" , "chai" , "oolong tea"];
 selectedTeas = [];
-
 for (let i = 0; i < teas.length; i++) {
      if (teas[i] === "chai") {
         break
     }
     selectedTeas.push(teas[i] 
+    )}
+// console.log(selectedTeas)
 
-    )
+let cities = ["London" , "New York" , "Paris" , "Berlin"];
+visitedCities = [];
 
-   
+for (let i = 0; i < cities.length; i++) {
+   if (cities[i] === "Paris") {
+    continue
+   }
 
+   visitedCities.push(cities[i])
+    
 }
-console.log(selectedTeas)
+console.log(visitedCities)
