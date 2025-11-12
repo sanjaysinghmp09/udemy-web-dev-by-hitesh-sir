aae11bd3abbe03ae9f5021036adfe3754cdc6c87
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-12 20:54:14 +0530

    start function lesring from here !

--- function/function.js
+++ function/function.js
@@ -1,4 +1,14 @@
 function greet (name) {
     console.log(`Hey ! ${name}`)
 }
-greet("Sanjay")
+// greet("Sanjay")
+
+function Car(brand, year) {
+  this.brand = brand;
+  this.year = year;
+  return year;
+}
+
+const myCar = new Car("BMW", 2001);
+
+console.log(myCar); // Car { brand: 'BMW', year: 2001 }
---
+++ function/function1.js
@@ -0,0 +1,5 @@
+function makeTea(typeOfTea) {
+    return `Making ${typeOfTea}`;
+}
+let teaMaker = makeTea('green tea') ;
+console.log(teaMaker)
