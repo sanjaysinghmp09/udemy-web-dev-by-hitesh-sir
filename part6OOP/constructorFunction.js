2eb124e11115955766205bf25b90eecd9c528f2a
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-17 20:29:16 +0530

    I write my second  Constructor Function

--- part6OOP/constructorFunction.js
+++ part6OOP/constructorFunction.js
@@ -7,4 +7,13 @@ greet('Sanjay')
 function Person (name , age){
     this.name = name 
     this.age = age 
+    console.log("Working first Person")
 }
+Person("Sanjay" , 12)
+
+function Person1 (username , password){
+    this.username = username ;
+    this.password = password
+    console.log("Working second Person")
+}
+Person1("sanjaysingh" , 'sanjay@123')
