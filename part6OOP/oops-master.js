fe0493158379050d76157d3926c4160c7e917be6
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-26 19:43:20 +0530

    Encapsulation try in my BAnk Account GetAmount Method

--- part6OOP/oops-master.js
+++ part6OOP/oops-master.js
@@ -1,5 +1,5 @@
 // Encapsulation 
-
+/*
 class BankAccount {
     #balance = 49253 ;
 
@@ -14,3 +14,43 @@ class BankAccount {
 let account = new BankAccount () ; 
 
 console.log(account.getBalance())
+*/
+
+function BankAccount(accountNumber, accountHolderName, balance) {
+    let _accountNumber = accountNumber;
+    let _accountHolderName = accountHolderName;
+    let _balance = balance;
+
+    function showAccountDetails() {
+        console.log(`Account Number: ${_accountNumber}`);
+        console.log(`Account Holder Name: ${_accountHolderName}`);
+        console.log(`Balance: ${_balance}`);
+    }
+
+    function deposit(amount) {
+        _balance += amount;
+        showAccountDetails();
+    }
+
+    function withdraw(amount) {
+        if (_balance >= amount) {
+            _balance -= amount;
+            showAccountDetails();
+        } else {
+            console.log("Insufficient Balance");
+        }
+    }
+
+    return {
+        deposit: deposit,
+        withdraw: withdraw
+    };
+}
+
+let myBankAccount = BankAccount("54400100008011", "Sanjay Singh", 50753);
+
+myBankAccount.deposit(500); 
+// Output: Account Number: 123456 Account Holder Name: 
+//John Doe Balance: 1500
+
+myBankAccount.withdraw(2000); // Output: Insufficient Balance
