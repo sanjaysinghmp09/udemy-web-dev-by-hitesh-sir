aba26ae87e097c6f590375e2613e88a638249ec4
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-28 08:52:38 +0530

    Changes in Bg Color

---
+++ part8/script.js
@@ -0,0 +1,6 @@
+// example 1
+
+document.getElementById("changeTextButton").addEventListener('click' , () => {
+  let paragraph = document.getElementById('myParagraph')
+  console.log(paragraph)
+})
