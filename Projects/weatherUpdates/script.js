e4534ddcb2aedf5bb3cc3934c3ec876ba6a0bbe4
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-06 07:49:12 +0530

    add fetch and error handling

--- Projects/weatherUpdates/script.js
+++ Projects/weatherUpdates/script.js
@@ -7,13 +7,29 @@ document.addEventListener('DOMContentLoaded' , () => {
   const descriptionDisplay = document.getElementById("description");
   const errorMessage = document.getElementById("error-message");
 
+  const API_KEY = "e91b54ada70640593a66e0490a5a2084"   // env variables
+
   getWeatherBtn.addEventListener("click" , () => {
     cityInput.value.trim()
     if(!city) return ;
 
-    
-
   })
+
+  function fetchWeatherData(city) {
+    // gets the data 
+  }
+
+  function displayWeatherData(weatherData) {
+    // display
+  }
+
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove('hidden')
+  }
+
+
+
 })
 
 
