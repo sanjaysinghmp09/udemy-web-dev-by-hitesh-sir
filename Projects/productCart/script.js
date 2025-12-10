591cda1377bda5f521475e351ad78a091d4577f3
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-10 19:18:45 +0530

    change obj add gaming vibe

--- Projects/productCart/script.js
+++ Projects/productCart/script.js
@@ -1,26 +1,27 @@
-
-document.addEventListener("DOMContentLoaded" , ()=> {
+document.addEventListener("DOMContentLoaded", () => {
   const products = [
-    {id : 1, name : "Product" , price: 29.99},
-    {id : 2, name : "Product" , price: 19.99},
-    {id : 3, name : "Product" , price: 59.99},
+    { id: 101, name: "Bronze Pack", price: 29.99 },
+    { id: 102, name: "Silver Pack", price: 49.99 },
+    { id: 103, name: "Gold Pack", price: 79.99 },
+    { id: 104, name: "Platinum  Pack", price: 109.99 },
+    { id: 105, name: "Diamond Pack", price: 159.99 },
   ];
 
-  const cart = []
-  const productList = document.getElementById("product-list") ;
-  const cartItems = document.getElementById('cart-items')
-  const emptyCartMessage = document.getElementById('empty-cart')
-  const cartTotalMessage = document.getElementById('total-price')
-  const checkout = document.getElementById('checkout-btn')
+  const cart = [];
+  const productList = document.getElementById("product-list");
+  const cartItems = document.getElementById("cart-items");
+  const emptyCartMessage = document.getElementById("empty-cart");
+  const cartTotalMessage = document.getElementById("total-price");
+  const checkout = document.getElementById("checkout-btn");
 
-  products.forEach ((product) => {
+  products.forEach((product) => {
     const productDiv = document.createElement("div");
     productDiv.classList.add("product");
     productDiv.innerHTML = `
     <span> ${product.name} - $ ${product.price.toFixed(2)} </span>
     <button data-id="${product.id}">Add to Cart</button>
-    ` ;
+    `;
 
-    productList.appendChild(productDiv)
+    productList.appendChild(productDiv);
   });
 });
