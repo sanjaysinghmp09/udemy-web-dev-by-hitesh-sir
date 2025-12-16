08aaee90040613a6dba12a68ca7bee93ff8d4111
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-16 20:18:47 +0530

    target all documents

--- Projects/productCart/script.js
+++ Projects/productCart/script.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded');
 const expenseForm = document.getElementById('expense-form');
 const expenseNameInput = document.getElementById('expense-name');
 const expenseAmountInput = document.getElementById('expense-amount');
-const expenseList = document.getElementById('expense-list');
+const expenseList = document.getElementById('expense-list'); 
+const totalAmountDisplay = document.getElementById('total-amount'); 
 
+function  msg() {
+    console.log("Just chiil")
+}
+msg()
 
