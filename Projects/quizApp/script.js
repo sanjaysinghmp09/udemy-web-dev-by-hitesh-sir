b13991e875455f2c819fa514a162436f5ad5c73c
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-21 20:09:42 +0530

    create new project - quiz app

---
+++ Projects/quizApp/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded' , () => {
+
+
+    const questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      choices: ["Mars", "Venus", "Jupiter", "Saturn"],
+      answer: "Mars",
+    },
+    {
+      question: "Who wrote 'Hamlet'?",
+      choices: [
+        "Charles Dickens",
+        "Jane Austen",
+        "William Shakespeare",
+        "Mark Twain",
+      ],
+      answer: "William Shakespeare",
+    },
+  ];
+})
