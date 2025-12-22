85f4d63366e28fc0d761ecaec8c6c668a39889a4
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-12-22 19:22:20 +0530

    build logic and create quiz app

--- Projects/quizApp/script.js
+++ Projects/quizApp/script.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded' , () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const startBtn = document.getElementById("start-btn");
+  const nextBtn = document.getElementById("next-btn");
+  const restartBtn = document.getElementById("restart-btn");
+  const questionContainer = document.getElementById("question-container");
+  const questionText = document.getElementById("question-text");
+  const choicesList = document.getElementById("choices-list");
+  const resultContainer = document.getElementById("result-container");
+  const scoreDisplay = document.getElementById("score");
 
-    const startBtn = document.getElementById("start-btn")
-    const nextBtn = document.getElementById("next-btn")
-    const restartBtn = document.getElementById("restart-btn")
-    const questionContainer = document.getElementById("question-container")
-    const questionText = document.getElementById("question-text")
-    const choicesList = document.getElementById("choices-list")
-    const resultContainer = document.getElementById("result-container")
-
-    const questions = [
+  const questions = [
     {
       question: "What is the capital of France?",
       choices: ["Paris", "London", "Berlin", "Madrid"],
@@ -30,4 +30,31 @@ document.addEventListener('DOMContentLoaded' , () => {
       answer: "William Shakespeare",
     },
   ];
-})
+
+  let currentQuestionIndex = 0 ;
+  let score = 0 
+
+  startBtn.addEventListener("click" , stratQuiz)
+
+  function stratQuiz() {
+    startBtn.classList.add('hidden');
+    resultContainer.classList.add('hidden')
+    questionContainer.classList.remove('hidden')
+    showQuestion()
+  }
+
+  function showQuestion() {
+    nextBtn.classList.add('hidden')
+    questionText.textContent = questions[currentQuestionIndex].question
+    choicesList.innerHTML = "" // clear previous choice
+    questions[currentQuestionIndex].choices.forEach(choice => {
+      const li = document.createElement('li')
+      li.textContent = choice 
+      li.addEventListener('click')
+
+    })
+  }
+  function selectAnswer(choice) {
+    
+  }
+});
