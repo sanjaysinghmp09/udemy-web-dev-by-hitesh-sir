0e47d728f277b26656c13521fb66e2fc588ab236
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-02-12 21:00:13 +0530

    call BAckFun add in server

--- nginxMyOwnServer/server.js
+++ nginxMyOwnServer/server.js
@@ -3,10 +3,12 @@ const fs = require("fs")
 const path = require("path")
 
 const port = 3000 ;
-const server = http.createServer();
+const server = http.createServer((req , res) => {
+
+});
 
 // crete own server using nginx
 
 server.listen(port , () => {
- console.log(`Server is lisning as port number ${port}`)
+ console.log(`Server is lisning on port number ${port}`)
 })
